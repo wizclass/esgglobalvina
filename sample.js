9cb5e14cf26ffce3a148649d2466c403c147fcf4
canoa <canoa0327@naver.com>
2023-11-25 18:22:59 +0900

    영어, 베트남어 지원 추가

---
+++ sample.js
@@ -0,0 +1,72 @@
+en = {
+  0: "By practicing carbon neutrality",
+  1: "A company taking the lead in overcoming the climate crisis",
+  2: `ESG GLOBAL VINA is the Vietnamese local branch of ESG GLOBAL, a leading carbon neutral company.
+  It is a corporation<br />
+  By operating a large-scale ‘Kenaf’ farm, an environmental purification plant,
+  We are contributing to reducing greenhouse gases.<br />`,
+  3: `Kenaf is an optimal feed crop rich in various nutrients,<br />
+  Activated carbon, secondary battery anode material, natural pulp, building materials, biodegradable plastic, etc.<br />
+  It can be used as a material for various eco-friendly products.`,
+  4: `ESG GLOBAL VINA contributes substantially to overcoming the climate crisis.
+  We are emerging as an eco-friendly company.`,
+  5: "Thank You",
+  6: "Kenaf cultivation",
+  7: `ESG GLOBAL VINA's kenaf farm is located in Binh Phuoc Province, Vietnam.
+  Yes.<br />
+  Up to 5 crops per year thanks to the combination of the area's fertile land and subtropical climate.
+  Production is possible. Rather than procuring seeds from outside,<br />
+  We operate our own kenaf seed farm to produce stable kenaf.
+  Cultivated.<br />
+  In addition, we established an annual farm expansion plan to continuously produce kenaf.
+  We are in the process of expanding the scale.`,
+  8: "Sowing",
+  9: "Culture",
+  10: "Harvesting",
+  11: "Feed manufacturing",
+  12: `Harvested kenaf is transported to a processing plant near the farm, crushed and dried.
+  Through this process, it is processed into pellets for livestock feed.<br />
+  Pellets have crude protein content that takes into account the tastes and nutritional components of various livestock.
+  It is produced accordingly.<br />
+  High quality based on ESG GLOBAL VINA’s unique feed manufacturing technology
+  We produce kenaf feed.`,
+  13: "Trituration",
+  14: "Drying",
+  15: "Pellet",
+  16: "Kenaf Farm",
+  17: `ESG GLOBAL VINA's kenaf farm is located in Binh Phuoc Province, Vietnam.
+  there is. Thanks to the combination of Vietnam's unique fertile soil and subtropical climate,
+  This is the optimal environment for up to 5 copies.<br />
+  ESG GLOBAL VINA produces quality seeds by owning its own seed farm.
+  Excellent quality feed and various eco-friendly products through stable supply
+  We produce raw materials.`,
+  18: "Farm",
+  19: "Harvesting",
+  20: "Business Status",
+  21: "feed mill",
+  22: "Kenaf",
+  23: `Grows up to 3m or more after 100 days of cultivation<br />
+  It has fast growth and high productivity compared to other feed crops.<br />
+  It has a wide range of adaptability to climate and soil, even in harsh environments<br />
+  The germination rate is excellent and the carbon dioxide absorption rate is higher than that of general crops<br />
+  The carbon reduction effect is excellent.`,
+  24: "Application field",
+  25: `Livestock feed, activated carbon, natural pulp, secondary battery anode material,<br />
+  Building materials, biodegradable plastics, etc.`,
+  26: "Kenaf feed nutritional value",
+  27: `Kenaf feed has a high crude protein content as well<br />
+  Omega 3, 6, 9 ingredients, polyphenol, calcium, etc.<br />
+  It is rich in various nutrients.<br />
+  Kenaf is highly palatable to livestock, including cattle, chickens, pigs, horses, etc.
+  Various<br />
+  It has high utility as livestock feed.`,
+  28: "Economic feasibility of kenaf feed",
+  29: `Kenaf has excellent dry matter yield (weight after the crop is completely dried),<br />
+  In terms of production, compared to general forage crops<br />
+  It boasts a superior level.`,
+  30: `If you have any questions, please enter your information. As soon as possible
+  We will contact you.`,
+  31: "I agree to the privacy policy and collection of personal information.",
+  32: "View full text",
+  33: "Contact",
+};
