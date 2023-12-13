f9c2bbdeeffd156806dd95bd3f53833e421911b2
SeongKon <canoa950327@gmail.com>
2023-12-13 18:48:57 +0900

    고객사 수정사항 반영

--- sample.js
+++ sample.js
@@ -1,72 +1,66 @@
-en = {
-  0: "By practicing carbon neutrality",
-  1: "A company taking the lead in overcoming the climate crisis",
-  2: `ESG GLOBAL VINA is the Vietnamese local branch of ESG GLOBAL, a leading carbon neutral company.
-  It is a corporation<br />
-  By operating a large-scale ‘Kenaf’ farm, an environmental purification plant,
-  We are contributing to reducing greenhouse gases.<br />`,
-  3: `Kenaf is an optimal feed crop rich in various nutrients,<br />
-  Activated carbon, secondary battery anode material, natural pulp, building materials, biodegradable plastic, etc.<br />
-  It can be used as a material for various eco-friendly products.`,
-  4: `ESG GLOBAL VINA contributes substantially to overcoming the climate crisis.
-  We are emerging as an eco-friendly company.`,
-  5: "Thank You",
-  6: "Kenaf cultivation",
-  7: `ESG GLOBAL VINA's kenaf farm is located in Binh Phuoc Province, Vietnam.
-  Yes.<br />
-  Up to 5 crops per year thanks to the combination of the area's fertile land and subtropical climate.
-  Production is possible. Rather than procuring seeds from outside,<br />
-  We operate our own kenaf seed farm to produce stable kenaf.
-  Cultivated.<br />
-  In addition, we established an annual farm expansion plan to continuously produce kenaf.
-  We are in the process of expanding the scale.`,
-  8: "Sowing",
-  9: "Culture",
-  10: "Harvesting",
-  11: "Feed manufacturing",
-  12: `Harvested kenaf is transported to a processing plant near the farm, crushed and dried.
-  Through this process, it is processed into pellets for livestock feed.<br />
-  Pellets have crude protein content that takes into account the tastes and nutritional components of various livestock.
-  It is produced accordingly.<br />
-  High quality based on ESG GLOBAL VINA’s unique feed manufacturing technology
-  We produce kenaf feed.`,
-  13: "Trituration",
-  14: "Drying",
-  15: "Pellet",
-  16: "Kenaf Farm",
-  17: `ESG GLOBAL VINA's kenaf farm is located in Binh Phuoc Province, Vietnam.
-  there is. Thanks to the combination of Vietnam's unique fertile soil and subtropical climate,
-  This is the optimal environment for up to 5 copies.<br />
-  ESG GLOBAL VINA produces quality seeds by owning its own seed farm.
-  Excellent quality feed and various eco-friendly products through stable supply
-  We produce raw materials.`,
-  18: "Farm",
-  19: "Harvesting",
-  20: "Business Status",
-  21: "feed mill",
+$.lang.vi = {
+  0: `<strong>công ty đi đầu vượt qua khủng hoảng khí hậu bằng thực hành trung hòa carbon <br /><strong>`,
+  1: "Công ty",
+  2: `ESG GLOBAL VINA là công ty con tại Việt Nam của ESG GLOBAL,<br /> 
+  một công ty hàng đầu về trung hòa carbon và đang góp phần giảm <br />
+  khí nhà kính bằng cách vận hành trang trại 'Kenaf' quy mô lớn, một nhà máy lọc môi trường.`,
+  3: `Kenaf là loại cây thức ăn chăn nuôi tối ưu, giàu chất dinh dưỡng và có thể<br />
+  được sử dụng làm nguyên liệu cho nhiều sản phẩm thân thiện với môi trường như than hoạt tính,<br />
+  vật liệu cực dương của pin thứ cấp, bột giấy tự nhiên,<br />
+  vật liệu xây dựng và nhựa phân hủy sinh học.`,
+  4: `ESG GLOBAL VINA đang nổi lên như một công ty thân thiện với môi trường,<br />
+   góp phần đáng kể vào việc vượt qua khủng hoảng khí hậu.`,
+  5: `Cám ơn`,
+  6: `Trồng Kenaf`,
+  7: `Trang trại kenaf của ESG GLOBAL VINA nằm ở tỉnh Bình Phước, Việt Nam.<br />
+  Sự kết hợp giữa đất đai màu mỡ và khí hậu cận nhiệt đới của khu vực cho phép sản xuất tới 5 vụ mỗi năm.<br /> 
+  Thay vì mua hạt giống từ bên ngoài, chúng tôi trồng kenaf một cách đáng tin cậy bằng cách vận hành trang trại hạt giống kenaf của riêng mình.<br />
+  Ngoài ra, chúng tôi đang trong quá trình liên tục mở rộng quy mô sản xuất kenaf bằng cách lập kế hoạch mở rộng trang trại hàng năm.`,
+  8: "Gieo hạt",
+  9: "Văn hoá",
+  10: "thu hoạch",
+  11: "Sản xuất thức ăn chăn nuôi",
+  12: `Kenaf thu hoạch được vận chuyển đến nhà máy chế biến gần trang trại, nghiền nát, sấy khô và chế biến thành dạng viên làm thức ăn chăn nuôi.<br />
+  Viên được sản xuất theo hàm lượng protein thô có tính đến sở thích và hàm lượng dinh dưỡng của các loại vật nuôi khác nhau.<br />
+  ESG GLOBAL VINA sản xuất thức ăn kenaf chất lượng cao dựa trên công nghệ sản xuất thức ăn chăn nuôi độc đáo của mình.`,
+  13: "đập vỡ",
+  14: "Sấy khô",
+  15: "Viên nhỏ",
+  16: "Trang trại Kenaf",
+  17: `Trang trại kenaf của ESG GLOBAL VINA nằm ở tỉnh Bình Phước, Việt Nam.
+  Sự kết hợp giữa đất đai màu mỡ độc đáo của Việt Nam và khí hậu cận nhiệt đới tạo ra môi trường tối ưu cho 5 vụ mùa mỗi năm.<br />
+  ESG GLOBAL VINA sở hữu trang trại giống riêng và sản xuất thức ăn và nguyên liệu thô chất lượng tuyệt vời cho nhiều sản phẩm thân thiện với môi trường thông qua nguồn cung cấp hạt giống chất lượng cao ổn định.`,
+  18: "Nông trại",
+  19: "thu hoạch",
+  20: "Tình trạng kinh doanh",
+  21: "nhà máy thức ăn chăn nuôi",
   22: "Kenaf",
-  23: `Grows up to 3m or more after 100 days of cultivation<br />
-  It has fast growth and high productivity compared to other feed crops.<br />
-  It has a wide range of adaptability to climate and soil, even in harsh environments<br />
-  The germination rate is excellent and the carbon dioxide absorption rate is higher than that of general crops<br />
-  The carbon reduction effect is excellent.`,
-  24: "Application field",
-  25: `Livestock feed, activated carbon, natural pulp, secondary battery anode material,<br />
-  Building materials, biodegradable plastics, etc.`,
-  26: "Kenaf feed nutritional value",
-  27: `Kenaf feed has a high crude protein content as well<br />
-  Omega 3, 6, 9 ingredients, polyphenol, calcium, etc.<br />
-  It is rich in various nutrients.<br />
-  Kenaf is highly palatable to livestock, including cattle, chickens, pigs, horses, etc.
-  Various<br />
-  It has high utility as livestock feed.`,
-  28: "Economic feasibility of kenaf feed",
-  29: `Kenaf has excellent dry matter yield (weight after the crop is completely dried),<br />
-  In terms of production, compared to general forage crops<br />
-  It boasts a superior level.`,
-  30: `If you have any questions, please enter your information. As soon as possible
-  We will contact you.`,
-  31: "I agree to the privacy policy and collection of personal information.",
-  32: "View full text",
-  33: "Contact",
+  23: `Nó có tốc độ tăng trưởng nhanh, cao tới 3 m hoặc hơn sau 100 ngày canh tác và có năng suất cao so với các loại cây thức ăn chăn nuôi khác.<br />
+  Nó có khả năng thích ứng rộng với khí hậu và đất đai nên có tỷ lệ nảy mầm tuyệt vời ngay cả trong môi trường khắc nghiệt, tốc độ hấp thụ carbon dioxide cao hơn so với các loại cây trồng thông thường nên có tác dụng giảm carbon tuyệt vời.`,
+  24: "Lĩnh vực ứng dụng",
+  25: `Thức ăn chăn nuôi, than hoạt tính, bột giấy tự nhiên, vật liệu cực dương pin thứ cấp, vật liệu xây dựng, nhựa phân hủy sinh học, v.v.`,
+  26: `Giá trị dinh dưỡng thức ăn Kenaf`,
+  27: `Thức ăn Kenaf rất giàu chất dinh dưỡng khác nhau như hàm lượng protein thô cao, thành phần omega 3, 6 và 9, polyphenol và canxi.<br />
+  Kenaf rất ngon miệng đối với vật nuôi và có công dụng cao làm thức ăn cho nhiều loại vật nuôi khác nhau, bao gồm gia súc, gà, lợn và ngựa.`,
+  28: `Tính khả thi về mặt kinh tế của thức ăn kenaf`,
+  29: `Kenaf có năng suất chất khô tuyệt vời (trọng lượng sau khi cây khô hoàn toàn) và tự hào có mức sản lượng vượt trội so với các loại cây làm thức ăn chăn nuôi thông thường.`,
+  30: `Nếu có thắc mắc vui lòng nhập thông tin. Chúng tôi sẽ liên hệ với bạn ngay khi có thể.`,
+  31: `Tôi đồng ý với chính sách bảo mật và thu thập thông tin cá nhân.`,
+  32: `Xem toàn văn`,
+  33: `Liên hệ chúng tôi`,
+  34: "Cam kết bảo mật",
+  35: `Để bảo vệ thông tin cá nhân của người dùng hoạt động trên trang web này (sau đây gọi là ‘trang web’), chúng tôi đã thiết lập và đang thực hiện chính sách xử lý thông tin cá nhân sau đây.`,
+  36: "■ Phạm vi thu thập thông tin cá nhân",
+  37: `1) Thông tin được nhập để nhận yêu cầu<br />
+  - Thông tin bắt buộc: tên, số điện thoại di động, số điện thoại, địa chỉ email, v.v.`,
+  38: "■ Mục đích thu thập và sử dụng thông tin cá nhân",
+  39: `Trang web thu thập thông tin cá nhân cho các mục đích sau.
+  <br />
+  <br />
+  [Đồng ý thu thập và sử dụng thông tin cá nhân của người không phải thành viên]<br />
+  - Mục đích: Xác nhận thắc mắc và xử lý/tư vấn yêu cầu thông qua tư vấn khách hàng 1:1 trên website <br />
+  - Mục: tên công ty, tên, thông tin liên lạc, địa chỉ email`,
+  40: "■ Thời gian lưu giữ và sử dụng thông tin cá nhân",
+  41: `Thông tin cá nhân của thành viên sẽ bị hủy sau khi đạt được mục đích thu thập hoặc cung cấp thông tin cá nhân như sau. <br />
+  - Thời gian: 6 tháng kể từ khi nhận được yêu cầu và hoàn thành tư vấn`,
 };
